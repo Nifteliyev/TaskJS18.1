57a398123ed1053e70d373abe3343965083639a4
Nifteliyev <aminnifteliyev45@gmail.com>
2025-04-04 19:10:02 +0400

    JSTask18.1

---
+++ app.js
@@ -0,0 +1,18 @@
+let table = document.querySelector("table");
+let tr = document.querySelector("tr");
+tr.innerHTML = '<td>Nifteliyev Amin</td>';
+table.append(tr);
+
+let tr1 = document.querySelector("tr");
+tr1.innerHTML = '<td>Nebiyeva Nigar</td>';
+table.append(tr1);
+
+
+
+let tr2 = document.querySelector("tr");
+tr2.innerHTML = '<td>Qudretli Daniz</td>';
+table.append(tr2);
+
+
+
+
