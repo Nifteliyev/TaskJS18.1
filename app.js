9810e3040b79b9f6c9606092b83a0f58ca125071
Nifteliyev <aminnifteliyev45@gmail.com>
2025-04-06 15:03:31 +0400

    JStask18.1

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-let table = document.querySelector("table");
-let tr = document.querySelector("tr");
-tr.innerHTML = '<td>Nifteliyev Amin</td>';
-table.append(tr);
-
-let tr1 = document.querySelector("tr");
-tr1.innerHTML = '<td>Nebiyeva Nigar</td>';
-table.append(tr1);
 
 
 
-let tr2 = document.querySelector("tr");
-tr2.innerHTML = '<td>Qudretli Daniz</td>';
-table.append(tr2);
+let ul = document.querySelector("ul");
+
+function addStudent(student){
+    let li = document.createElement("li");
+    li.innerHTML = student;
+    ul.append(li);
+
+    
+}
+addStudent("Nebiyeva Nigar")
+addStudent("Nebiyeva Nigar")
+addStudent("Nebiyeva Nigar")
+
 
 
 
